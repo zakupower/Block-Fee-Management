b12b8f2104f9533be10d61bb7b8c58fa3bb3ffd1
Dimitar Ganev <pwnm3rcury@gmail.com>
2016-11-15 21:49:49 +0200

    1
    11

--- src/connection.php
+++ src/connection.php
@@ -1,32 +1,11 @@
 <?php
 /* da ne zabravim chmod 644 na config.php */
 require ("config.php");
-<<<<<<< HEAD
-
-class Database {
-
-    public $conn = null;
-
-    public function connect()
-    {
-      try
-      {
-          $this->conn = new PDO("mysql:host=".DB_HOSTNAME.";dbname=".DB_DATABASE."",DB_USERNAME,DB_PASSWORD);
-          $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      }
-      catch(PDOException $e)
-      {
-          echo $e->getMessage();
-      }
-      return $this->conn;
-    }
-=======
-public class DatabaseConnect {
-  function __construct()
+class DatabaseConnect {
+  public function __construct()
   {
       mysql_connect(DB_HOSTNAME,DB_USERNAME,DB_PASSWORD) or die('Could not connect to MySQL/MariaDB server.');
       mysql_select_db(DB_DATABASE);
   }
->>>>>>> parent of bf319e6... hevy shit
 }
 ?>
