a2c17ce53eff50fdc88c5c786719e38a277e7a0c
Dimitar Ganev <pwnm3rcury@gmail.com>
2016-11-15 14:18:34 +0200

    Create db.sql

---
+++ db.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE db_block;
+CREATE TABLE users
+(
+  user_ID INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(50) NOT NULL,
+  password CHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  premissions INT NOT NULL -- 1- User / 2- Domoupravitel / 3- Admin
+);
+
+CREATE TABLE vhod 
+(
+  taksa_asansior DECIMAL NOT NULL,
+  taksa_vhod DECIMAL NOT NULL,
+  taska_chistachka DECIMAL NOT NULL,
+  taksa_dop DECIMAL NOT NULL,
+  vhod_pari DECIMAL NOT NULL,
+);
