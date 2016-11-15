d7c85e0ac072a2bc4f5a3dfd4f6830001b8b99fd
Dimitar Ganev <pwnm3rcury@gmail.com>
2016-11-15 14:41:38 +0200

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,24 +1,32 @@
 CREATE DATABASE db_block;
 CREATE TABLE users
 (
-  user_ID INT NOT NULL AUTO_INCREMENT,
-  username VARCHAR(50) NOT NULL,
-  password CHAR(50) NOT NULL,
-  email VARCHAR(50) NOT NULL,
-  first_name VARCHAR(50) NOT NULL,
-  last_name VARCHAR(50) NOT NULL,
-  premissions INT NOT NULL, -- 1- User / 2- Domoupravitel / 3- Admin
-  etaj INT NOT NULL,
-  apartament INT NOT NULL,
-  PRIMARY KEY (user_ID)
+  users_ID INT NOT NULL AUTO_INCREMENT,
+  users_username VARCHAR(50) NOT NULL,
+  users_password CHAR(50) NOT NULL,
+  users_email VARCHAR(50) NOT NULL,
+  users_first_name VARCHAR(50) NOT NULL,
+  users_last_name VARCHAR(50) NOT NULL,
+  users_premissions INT NOT NULL, -- 1- User / 2- Domoupravitel / 3- Admin
+  users_etaj INT NOT NULL,
+  users_apartament INT NOT NULL,
+  PRIMARY KEY (users_ID)
 );
 
 CREATE TABLE vhod 
 (
-  taksa_asansior DECIMAL NOT NULL,
-  taksa_vhod DECIMAL NOT NULL,
-  taska_pet DECIMAL NOT NULL,
-  taska_chistachka DECIMAL NOT NULL,
-  taksa_dop DECIMAL NOT NULL,
+  vhod_taksa_asansior DECIMAL NOT NULL,
+  vhod_taksa_vhod DECIMAL NOT NULL,
+  vhod_taska_pet DECIMAL NOT NULL,
+  vhod_taska_chistachka DECIMAL NOT NULL,
+  vhod_taksa_dop DECIMAL NOT NULL,
   vhod_pari DECIMAL NOT NULL,
 );
+
+CREATE TABLE msg -- za nqkvi sabraniq i etc da se poqvqva nai otgore na raotite
+(
+  msg_ID INT NOT NULL AUTO_INCREMENT,   
+  msg_type VARCHAR(20) NOT NULL,
+  msg_text TEXT NOT NULL
+);
+  
