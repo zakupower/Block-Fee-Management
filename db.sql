eebc7941baccedd9162cfb98db9de230cb1882c5
Dimitar Ganev <pwnm3rcury@gmail.com>
2016-11-15 14:35:38 +0200

    Update db.sql

--- db.sql
+++ db.sql
@@ -7,14 +7,17 @@ CREATE TABLE users
   email VARCHAR(50) NOT NULL,
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
-  pet INT NOT NULL, -- 0 - nqma 1- ima 
-  premissions INT NOT NULL -- 1- User / 2- Domoupravitel / 3- Admin
+  premissions INT NOT NULL, -- 1- User / 2- Domoupravitel / 3- Admin
+  etaj INT NOT NULL,
+  apartament INT NOT NULL,
+  PRIMARY KEY (user_ID)
 );
 
 CREATE TABLE vhod 
 (
   taksa_asansior DECIMAL NOT NULL,
   taksa_vhod DECIMAL NOT NULL,
+  taska_pet DECIMAL NOT NULL,
   taska_chistachka DECIMAL NOT NULL,
   taksa_dop DECIMAL NOT NULL,
   vhod_pari DECIMAL NOT NULL,
