cef2336e05318831be457a485b1f2c081bdd0b50
Dimitar Ganev <pwnm3rcury@gmail.com>
2016-11-15 22:00:09 +0200

    login prototype
    1

--- src/connection.php
+++ src/connection.php
@@ -2,7 +2,7 @@
 /* da ne zabravim chmod 644 na config.php */
 require ("config.php");
 class DatabaseConnect {
-  public function __construct()
+  public function login()
   {
       mysql_connect(DB_HOSTNAME,DB_USERNAME,DB_PASSWORD) or die('Could not connect to MySQL/MariaDB server.');
       mysql_select_db(DB_DATABASE);
