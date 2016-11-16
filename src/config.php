f2391ee2885d16b1ce4b65dad2cf806b3dd0b998
Dimitar Ganev <pwnm3rcury@gmail.com>
2016-11-17 00:54:43 +0200

    1
    1

--- src/config.php
+++ src/config.php
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
-<?php
-/* MySQL/MariaDB Settings */
-
-
-define (DB_USERNAME, "root");
-define (DB_PASSWORD, "");
-define (DB_DATABASE, "db_block_fee_management");
-=======
-/* MySQL/MariaDB Settings */
-
 <?php
 define (DB_USERNAME, "mysql_username");
 define (DB_PASSWORD, "mysql_password");
 define (DB_DATABASE, "database_name");
->>>>>>> parent of bf319e6... hevy shit
 define (DB_HOSTNAME, "localhost");
 ?>
