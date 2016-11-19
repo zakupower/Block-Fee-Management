82eb0f0f6ac5a31d5aff01c92f6b0eaf6b64aa9f
Dimitar Ganev <pwnm3rcury@gmail.com>
2016-11-19 14:12:27 +0200

    upd
    upd

---
+++ src/modules/upravlenie_na_jivushti/usercp/apartments.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/octicons/3.1.0/octicons.min.css">
+    <link rel="stylesheet" href="style.css">
+    <!--[if lt IE 9]>
+      <script src="https://cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+      <div class="container">
+      <hr />
+      <a href="profile.php">Профил</a> |
+      <a href="add_entrance.php">Добави Вход</a> |
+      <a href="add_apartment.php">Добави Апартамент</a> 
+      <hr />
+      <a href="entrance.php">Обратно</a>
+      <hr />
+      <div class="container">
+          <div class="row col-md-12 custyle">
+              <h2><a href="">сем. Иванови </a></h2>
+              <hr />
+          </div>
+      </div>
+
+    <script src="https://cdn.jsdelivr.net/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ src/modules/upravlenie_na_jivushti/usercp/entrance.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/octicons/3.1.0/octicons.min.css">
+    <link rel="stylesheet" href="style.css">
+    <!--[if lt IE 9]>
+      <script src="https://cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+      <div class="container">
+      <hr />
+      <a href="profile.php">Профил</a> |
+      <a href="add_entrance.php">Добави Вход</a> |
+      <a href="add_apartment.php">Добави Апартамент</a>
+      <hr />
+      <a href="index.php">Обратно</a>
+      <hr />
+      <div class="container">
+    <div class="row col-md-12 custyle">
+    <table class="table table-striped custab">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Име</th>
+            <th>Етаж</th>
+            <th>Апартамент</th>
+            <th>Живущи</th>
+            <th></th>
+        </tr>
+    </thead>
+        <tr>
+            <td>1</td>
+            <td><a href="apartments.php">сем. Иванови</a></td>
+            <td>3</td>
+            <td>1</td>
+            <td>3</td>
+            <td><a href="edit.php">Редактирай</a></td>
+        </tr>
+    </table>
+    </div>
+</div>
+  </div>
+
+    <script src="https://cdn.jsdelivr.net/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  </body>
+</html>
