10c26f1a9157e2d8dd7e0695d7a888cdfb15c7bb
Ivan Batakov <mmdollar@users.noreply.github.com>
2016-11-20 11:21:29 +0200

    Update config.php

--- src/modules/login/includes/config.php
+++ src/modules/login/includes/config.php
@@ -2,7 +2,10 @@
 ob_start();
 session_start();
 
-//set timezone
+// админнистратора който ще одобрява всичко домоуправители
+$adminEmail = "mmdollar@gmail.com";
+
+// да си сложим и нашата времева зона :)
 date_default_timezone_set('Europe/Sofia');
 
 //database credentials
@@ -11,23 +14,23 @@ define('DBUSER','test');
 define('DBPASS','testtest');
 define('DBNAME','test_db');
 
-//application address
+// адреси на системата
 define('DIR','http://block.selendis.pw/');
 define('SITEEMAIL','no-reply@block.selendis.pw');
 
 try {
 
-	//create PDO connection
+	// създаваме PDO връзка
 	$db = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch(PDOException $e) {
-	//show error
+	// показване на грешки
     echo '<p class="bg-danger">'.$e->getMessage().'</p>';
     exit;
 }
 
-//include the user class, pass in the database connection
+// include include include :D
 include('classes/user.php');
 include('classes/phpmailer/mail.php');
 $user = new User($db);
