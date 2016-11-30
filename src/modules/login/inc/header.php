58d9158642052b1f90f804d583cd230039d23af4
Ivan Batakov <mmdollar@users.noreply.github.com>
2016-11-30 21:33:17 +0200

    Update header.php

--- src/modules/login/inc/header.php
+++ src/modules/login/inc/header.php
@@ -4,6 +4,6 @@
     <meta charset="utf-8">
     <title><?php if(isset($title)){ echo $title; }?></title>
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="style/main.css">
+    <link rel="stylesheet" href="style/login.css">
 </head>
 <body>
