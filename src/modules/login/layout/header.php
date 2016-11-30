1f4bb31d3e1ae820c15f0a5f3923028c71ab96a1
Ivan Batakov <mmdollar@users.noreply.github.com>
2016-11-30 21:30:37 +0200

    Delete header.php

--- src/modules/login/layout/header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title><?php if(isset($title)){ echo $title; }?></title>
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="style/main.css">
-</head>
-<body>
